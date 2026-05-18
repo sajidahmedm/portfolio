ad9f41258dcb638a862f3258b8eabfd9277d7e45
Mohammed Sajid Ahmed <sajidahmedm383@gmail.com>
2026-05-19 04:20:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-
+ 
     sections.forEach(sec => {
 
         let top = window.scrollY;
